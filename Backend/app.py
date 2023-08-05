2cb7c4cf402f31f6cb3f7c7b28754b0b4f461790
AgileBite <agilebite.3@gmail.com>
2023-08-05 19:33:52 +0530

    added new code

--- Backend/app.py
+++ Backend/app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     message = 'Hello, World!'
     return render_template('index.html', message=message)
+
+@app.route('/add_inventory')
+def add_inventory():
+    return render_template('add_data.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
